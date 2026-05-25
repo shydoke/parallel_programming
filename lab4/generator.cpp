d3a563b08024f9d4f99c59818743c9430840de35
shydoke <ban33665@gmail.com>
2026-05-25 23:39:04 +0400

    Add matrices

---
+++ lab4/generator.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+    int n = 2000;
+
+    ofstream file("B_2000.txt");
+
+    file << n << '\n';
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            file << rand() % 10 << ' ';
+        }
+        file << '\n';
+    }
+
+    file.close();
+
+    return 0;
+}
